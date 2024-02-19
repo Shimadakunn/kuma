c4d1a9053fce9ef1539d3f3b56e8039b7dab47cb
Shimadakunn <lcombaret@hotmail.co.jp>
2024-02-19 11:07:05 +0100

    Profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,7 +5,7 @@ import { use, useEffect, useState } from "react";
 
 
 export default function Home() {
-  const { provider, address, logout, getPrivateKey, getAddresses } = useWeb3Auth();
+  const { provider, user, logout, getPrivateKey, getAddresses } = useWeb3Auth();
   const [privateKey, setPrivateKey] = useState<string[]>([]);
   const [addresses, setAddresses] = useState<string[]>([]);
   async function pk() {
@@ -18,6 +18,7 @@ export default function Home() {
       const getAdd = async () => {
         const addresses = await getAddresses();
         setAddresses(addresses);
+        console.log(user)
       }
       getAdd();
     }
@@ -25,8 +26,9 @@ export default function Home() {
   return(
     <>
       <div>
-        <p className="font-light">EVM Address <span className="font-normal">{addresses[0]}</span></p>
-        <p className="font-light">SOL Adress <span className="font-normal">{addresses[1]}</span></p>
+        <p className="font-light">EVM Address <span className="font-bold">{addresses[0]}</span></p>
+        <p className="font-light">SOL Adress <span className="font-bold">{addresses[1]}</span></p>
+        <p className="font-light">Email: <span className="font-bold">{user.email}</span></p>
         {/* <div className="font-extralight tracking-tight">{privateKey}</div> */}
         <Button onClick={logout} className="border-2 border-foreground bg-primary text-foreground">Logout</Button>
         <Button onClick={pk} className="border-2 border-foreground bg-primary text-foreground">Private key</Button>

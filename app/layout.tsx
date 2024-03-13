0483c0e4b36a289fbd323d6dbe63e3b2d8d09009
Shimadakunn <lcombaret@hotmail.co.jp>
2024-03-13 11:01:29 +0100

    deploying all without header

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,15 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        {/* <Web3AuthProvider> */}
+        <Web3AuthProvider>
           <div className="flex">
-            {/* <Footer /> */}
+            <Footer />
             <div className="flex flex-col w-full">
-              <Header />
+              {/* <Header /> */}
               {children}
             </div>
           </div>
-        {/* </Web3AuthProvider> */}
+        </Web3AuthProvider>
         <Toaster richColors/>
       </body>
     </html>
